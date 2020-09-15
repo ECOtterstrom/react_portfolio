29491f14d21bd44eb9bd4c9c0fae9b40cb806aed
Erin Otterstrom <hapiotter@gmail.com>
2020-09-15 12:20:04 -0600

    resolved routing issues

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
 import Nav from './components/Nav';
 import Jumbotron from './components/Jumbotron';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import NoMatch from './pages/NoMatch';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import NoMatch from './components/NoMatch';
 import Footer from './components/Footer';
-import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
     <Router>
-      <div>
-        <Nav />
-        <Jumbotron />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/Projects" component={Projects} />
-        <Route exact path="/Contact" component={Contact} />
-        <Route exact path="/NoMatch" component={NoMatch} />
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Nav />
+      <Jumbotron />
+      <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/Projects" component={Projects} />
+      <Route exact path="/Contact" component={Contact} />
+      <Route exact path="/NoMatch" component={NoMatch} />
+      </Switch>
+      <Footer />
+    </div>
+  </Router>
   );
 }
 
